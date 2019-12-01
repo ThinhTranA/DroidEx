24ba984bd2de24607c448607dfca2aab80947346
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2019-12-01 11:05:31 +0800

    Add animations

--- FitnessApp/app/src/main/java/com/visualsharp/fitnessapp/MainActivity.java
+++ FitnessApp/app/src/main/java/com/visualsharp/fitnessapp/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,11 +15,21 @@ public class MainActivity extends AppCompatActivity {
     TextView titlepage;
     TextView subtitlepage, btnexercise;
     ImageView imgpage;
+    Animation animimgpage, bttone, bttwo, btthree, ltr;
+    View bgprogress, bgprogresstop;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //load animation
+        animimgpage = AnimationUtils.loadAnimation(this, R.anim.animimgpage);
+        bttone = AnimationUtils.loadAnimation(this, R.anim.bttone);
+        bttwo = AnimationUtils.loadAnimation(this,R.anim.bttwo);
+        btthree = AnimationUtils.loadAnimation(this,R.anim.btthree);
+        ltr = AnimationUtils.loadAnimation(this, R.anim.ltr);
 
         //import font
         Typeface MLight = Typeface.createFromAsset(getAssets(), "fonts/MLight.ttf");
@@ -26,11 +39,23 @@ public class MainActivity extends AppCompatActivity {
         titlepage = (TextView) findViewById(R.id.titlepage);
         subtitlepage = (TextView) findViewById(R.id.subtitlepage);
         btnexercise = (TextView) findViewById(R.id.btnexercise);
+        imgpage = (ImageView) findViewById(R.id.imgpage);
+        bgprogress = (View) findViewById(R.id.bgprogress);
+        bgprogresstop = (View) findViewById(R.id.bgprogresstop);
 
         //customize font
         titlepage.setTypeface(Vidaloka);
         subtitlepage.setTypeface(MLight);
         btnexercise.setTypeface(MMedium);
+
+        //export animate
+        imgpage.startAnimation(animimgpage);
+        titlepage.startAnimation(bttone);
+        subtitlepage.startAnimation(bttone);
+
+        btnexercise.startAnimation(btthree);
+        bgprogress.startAnimation(bttwo);
+        bgprogresstop.startAnimation(ltr);
 
     }
 }
