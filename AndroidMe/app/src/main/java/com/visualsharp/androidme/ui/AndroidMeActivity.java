2ce456da1f734e4b1e104475bb0cb6d7a326479c
Thinh Tran <thinhtran@outlook.com.au>
2020-07-29 22:34:09 +0800

    Init

---
+++ AndroidMe/app/src/main/java/com/visualsharp/androidme/ui/AndroidMeActivity.java
@@ -0,0 +1,17 @@
+package com.visualsharp.androidme.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.visualsharp.androidme.R;
+
+public class AndroidMeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_me);
+    }
+}
