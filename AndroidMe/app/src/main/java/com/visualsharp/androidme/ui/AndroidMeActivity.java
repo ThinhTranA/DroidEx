fd8cdca26d38865492b554d17e15d8be579ca139
Thinh Tran <thinhtran@outlook.com.au>
2020-07-30 22:21:30 +0800

    Create response to click.

--- AndroidMe/app/src/main/java/com/visualsharp/androidme/ui/AndroidMeActivity.java
+++ AndroidMe/app/src/main/java/com/visualsharp/androidme/ui/AndroidMeActivity.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
 
 import com.visualsharp.androidme.R;
+import com.visualsharp.androidme.data.AndroidImageAssets;
 
 public class AndroidMeActivity extends AppCompatActivity {
 
@@ -15,7 +16,24 @@ public class AndroidMeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_me);
 
+        if(savedInstanceState == null) {
+
+
         BodyPartFragment headFragment = new BodyPartFragment();
+
+        headFragment.setImageIds(AndroidImageAssets.getHeads());
+        headFragment.setListIndex(1);
+
+        BodyPartFragment bodyFragment = new BodyPartFragment();
+
+        bodyFragment.setImageIds(AndroidImageAssets.getBodies());
+        bodyFragment.setListIndex(1);
+
+        BodyPartFragment legFragment = new BodyPartFragment();
+
+        legFragment.setImageIds(AndroidImageAssets.getLegs());
+        legFragment.setListIndex(1);
+
 
         FragmentManager fragmentManager = getSupportFragmentManager();
 
@@ -23,5 +41,14 @@ public class AndroidMeActivity extends AppCompatActivity {
                 .add(R.id.head_container, headFragment)
                 .commit();
 
+        fragmentManager.beginTransaction()
+                .add(R.id.body_container, bodyFragment)
+                .commit();
+
+        fragmentManager.beginTransaction()
+                .add(R.id.leg_container, legFragment)
+                .commit();
+
+        }
     }
 }
--- AndroidMe/app/src/main/java/com/visualsharp/androidme/ui/BodyPartFragment.java
+++ AndroidMe/app/src/main/java/com/visualsharp/androidme/ui/BodyPartFragment.java
@@ -1,6 +1,7 @@
 package com.visualsharp.androidme.ui;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,16 +14,67 @@ import androidx.fragment.app.Fragment;
 import com.visualsharp.androidme.R;
 import com.visualsharp.androidme.data.AndroidImageAssets;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
+
 public class BodyPartFragment extends Fragment {
+
+    public static final String IMAGE_ID_LIST = "image_ids";
+    public static final String LIST_INDEX = "list_index";
+    private static final String TAG = "BodyPartSegment";
+
+    private List<Integer> mImageIds;
+    private int mListIndex;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        if(savedInstanceState != null){
+            mImageIds = savedInstanceState.getIntegerArrayList(IMAGE_ID_LIST);
+            mListIndex = savedInstanceState.getInt(LIST_INDEX);
+        }
+
         View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
 
-        ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+        final ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
 
-        imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
+        if(mImageIds != null){
+            imageView.setImageResource(mImageIds.get(mListIndex));
+
+            imageView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    if(mListIndex < mImageIds.size() -1){
+                        mListIndex++;
+                    } else {
+                        mListIndex = 0;
+                    }
+                    imageView.setImageResource(mImageIds.get(mListIndex));
+                }
+            });
+
+        } else {
+            Log.v(TAG, "This fragment as a null list of image ids;");
+        }
 
         return rootView;
+    }
+
+    public void setImageIds(List<Integer> imageIds){
+        mImageIds = imageIds;
+    }
+
+    public void setListIndex(int index){
+        mListIndex = index;
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle currentState) {
+        currentState.putIntegerArrayList(IMAGE_ID_LIST, (ArrayList<Integer>) mImageIds);
+        currentState.putInt(LIST_INDEX, mListIndex);
     }
 }
